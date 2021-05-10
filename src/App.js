708fc6879728f32b1b990b98a646c4ccac85dcf4
Willdooo <wollmada@gmail.com>
2021-05-10 20:45:23 +0200

    Well...this was a failure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from "react";
 import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <div>pog</div>
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
